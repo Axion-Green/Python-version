3783e2e93f75a82b9dd152a3cfba1e1ef2dd9225
Henrique Pontes <126187491+Pontessxx@users.noreply.github.com>
2023-05-10 21:12:03 -0300

    Update codigo py.py
    v1.2.0
    Using def and mat.plot.list 
    add for in meses

--- codigo py.py
+++ codigo py.py
@@ -3,56 +3,68 @@
 #a media deve se encontrar em uma escala de 1 a 10 sendo 1 o ar mais limpo e 10 o ar mais poluido
 #a escala funciona classificando os valores de 1 a 3.33 o ar pouco poluído, de 3.33 a 6.66 o ar com poluição média, e entre 6.66 a 10 o ar muito poluído
 
+import matplotlib.pyplot as plt
 
-print('__')
-print('_ AXION GREEN ')
-print('__')
-print('\nAQUI ESTA NOSSO CODIGO EM VERSÃO PITHON')
-print('\nUTILIZAMOS FERRAMENTAS VISTAS EM AULA')
+dados = []
+meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ags', 'Set', 'Out', 'Nov', 'Dez']
+count = 0
+opcao = 0
 
-contador = 0
-while contador < 1000:
-    contador += 1
-    
-    ar = int(input('Media da qualidade do ar fornecida pelo sensor: '))
+def exibir_menu():
+    print('_______________')
+    print('- AXION GREEN -')
+    print('_______________')
+    print('\nAQUI ESTA NOSSO CODIGO EM VERSÃO PITHON')
+    print('\nUTILIZAMOS FERRAMENTAS VISTAS EM AULA')
+    print('\nEscolha uma opção:')
+    print('(1) Inserir dados')
+    print('(2) Ver gráfico')
 
-    #console no arduino é o LCD mas qui pode ser o propio console
+def plotar_grafico():
+    #gera o grafico no final do codigo
+    plt.plot(dados, color='darkcyan', label='Leituras de cada mês')
+    plt.title('Qualidade do ar - Anual')
+    plt.xlabel('Meses')
+    plt.ylabel('Valor')
+    plt.xticks(range(12), meses)
+    plt.yticks(range(10))
+    plt.legend()
+    plt.show()
 
+exibir_menu()
 
+while opcao != 2:
+    opcao = int(input('Opção: '))
+    if opcao == 1:
+        for i in range(12):
+            mes = meses[i]
+            ar = int(input(f'{mes}:\\nMédia mensal da qualidade do ar fornecida pelo sensor: '))
 
-    if 1 <= ar < 3:
-        ar = 3
-        led = 'VERDE'
-        buzzer = 'Notone'
-    elif  3 <= ar < 6:     #estrutura que nomeia as classes
-        ar = 6              # e define qual é o comportamento das leds e do buzzer
-        led = 'AMARELO'
-        buzzer = 'Notone'      
-    elif 6 <= ar <= 10:
-        ar = 9
-        led = 'VERMELHO'
-        buzzer = 'Tone'
+            if 1 <= ar < 3:
+                ar = 3
+                led = 'VERDE'
+                buzzer = 'Notone'
+            elif 3 <= ar < 6:
+                ar = 6
+                led = 'AMARELO'
+                buzzer = 'Notone'
+            elif 6 <= ar <= 10:
+                ar = 9
+                led = 'VERMELHO'
+                buzzer = 'Tone'
+            else:
+                print("Digite um número entre 1 e 10")
+                continue
+
+            dados.append(ar)
+            count += 1
+
+            if count == 12:
+                break
+    elif opcao == 2:
+        print('\\nGráfico:')
+        plotar_grafico()
     else:
-        print("digite um numero entre 1 e 10")
-
-    if ar == 3:
-        print('Ar com poluição baixa')
-        print(f'cor do led: {led}')           #verificando a variável ar e exibindo o comportamento dos componenes
-        print(f'Buzzer: {buzzer}')             
-    elif ar == 6:
-        print('Ar com poluição média')
-        print(f'cor do led: {led}')
-        print(f'Buzzer: {buzzer}')
-    elif ar == 9:
-        print('Ar com poluição crítica!')
-        print(f'cor do led: {led}')
-        print(f'Buzzer: {buzzer}')
-    else:
-        print('erro')
-    opcao = int(input('escolha uma opção \n (1) continuar \n (2) sair \n'))
-    if opcao == 2:
-        print('sair do codigo')          #oferecendo ao usuário a opção de continuar rodando o programa ou sair caso já esteja satisfeito
-        break      
-    
-
+        print('Opção inválida. Por favor, escolha novamente.')
+        
 #A partir daqui é necessária uma estrutura que seja capaz de ativar os diferentes LEDs da protoboard, seguindo a escala de índice de poluição
